149670cac46260670f10ecb793892913a123b58a
Ruben Alvarez Aragones <131602064+Ralar-14@users.noreply.github.com>
2023-05-09 13:18:54 +0200

    Creació de estructura principal del programa #2

---
+++ __main__.py
@@ -0,0 +1,24 @@
+from pytokr import items, item
+# ...importar les classes que calgui, fetes per vosaltres
+# ...inicialitzacions
+instruccio = item()
+while instruccio != 'fi':
+    if instruccio == 'experiment':
+        pass
+    # ...llegir dades addicionals i processar 'experiment'
+    elif instruccio == 'afegir_tret':
+        pass
+    # ...llegir dades addicionals i processar 'afegir_tret'
+    elif instruccio == 'treure_tret': # només grups 3 persones
+        pass
+    # ...llegir dades addicionals i processar 'treure_tret'
+    elif instruccio == 'consulta_tret':
+        pass
+    # ...llegir dades addicionals i processar 'consulta_tret'
+    elif instruccio == 'consulta_individu':
+        pass
+    # ...llegir dades addicionals i processar 'consulta_individu'
+    elif instruccio == 'distribucio_tret':
+        pass
+    # ...llegir dades addicionals i processar 'distribucio_tret'
+    instruccio = item()
